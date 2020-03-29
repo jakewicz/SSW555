08d1621295ffae301bf9af0565c7cf04a9844fb4
jakewicz <33581158+jakewicz@users.noreply.github.com>
2020-03-29 18:36:24 -0400

    JS User Stories Automated Tests

--- AutomatedTests2.py
+++ AutomatedTests2.py
@@ -51,6 +51,42 @@ class Test(unittest.TestCase):
 		}
 		families = {'@F1@': {'CHIL': ['@I1@', '@I2@', '@I3@', '@I4@']}}
 		self.assertEqual(US32_list_multiple_births(individuals, families), None)
+	    
+	def test_US01(self):
+        	individuals = {'@I1@':{'BIRT:13 Feb 2022'}}
+        	self.assertTrue(US01_check_current_date('@I1@', individuals))
+		
+   	def test_US10(self):
+        	individuals = {'@I1@':{'MARR_AGE': 12}}
+        	self.assertTrue(US10_young_marriage('@I1@', individuals))
+		
+    	def test_US16(self):
+        	individuals = {'@I1@':{'SEX': 'F'}}
+        	self.AssertTrue(US16_get_last_names('@I1@',individuals))
+		
+    	def test_US17(self):
+        	individuals = {
+            	'@I1@': {'FAMS':'@F2@'},
+            	'@I2@': {'FAMS':'@F2@'}
+        	}
+        	families = {'@F1@': {'HUSB': '@I1@', 'WIFE': '@I3@', 'CHIL': '@I3@'}}
+        	self.assertTrue(US17_dont_marry_children('@I1@', individuals, families))
+		
+    	def test_US18(self):
+        	individuals = {
+            	'@I1@': {'FAMC':'@F1@'},
+            	'@I2@': {'FAMC':'@F1@'}
+        	}
+        	families = {'@F1@': {'HUSB': '@I1@', 'WIFE': '@I2@'}}
+        	self.assertTrue(US18_siblings_should_not_marry('@I1@', individuals, families))
+		
+    	def test_US30(self):
+        	individuals = {
+            	'@I1@': {'FAMS':'N/A', 'DEAT':'5 OCT 2016'},
+            	'@I2@': {'FAMS':'N/A', 'DEAT': 'N/A'},
+            	'@I3@': {'FAMS': '@I4@', 'DEAT': 'N/A'}
+        	}
+        	self.assertTrue(US30_list_living_married(individuals))
 
 
 # needs to be at the end, don't touch this
