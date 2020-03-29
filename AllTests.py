9ccf55b03e829387ddee8a17456d60d6f66a3f84
Dan Bianchini <danbianchini64@gmail.com>
2020-03-29 16:05:43 -0400

    Fixed AllTests.py

--- AllTests.py
+++ AllTests.py
@@ -4,6 +4,9 @@ from Sprint01 import *
 from Sprint03 import *
 from Dan_Bianchini_User_Stories import *
 from Jacob_Senkewicz_User_Stories import *
+import Parser
+
+individuals, families = Parser.read_file("./test.ged")
 
 #sprint 1 tests
 def US02_test_born_after_married(indi, individuals):
@@ -16,27 +19,27 @@ def US03_test_death_before_birth(indi, individuals):
 
 def US07_test_check150(indi, individual):
     if (US07_check150(indi, individual) == "Error: Too Old"):
-        print("ERROR: US07", indi, ": Individual is over 150 years old ")
+        print("ERROR: US07", individuals[indi]['NAME'], ": Individual is over 150 years old ")
 
 def US06_test_divorce_before_death(indi, individuals):
     if(US06_divorce_before_death(indi, individuals) == "Error: cannot get divorced after death"):
-        print("ERROR: US06", indi, ": cannot get divorced after death")
+        print("ERROR: US06", individuals[indi]['NAME'], ": cannot get divorced after death")
 
 def US10_test_young_marriage(indi, individuals):
     if (US10_young_marriage(indi, individuals) == "Error: Too young to get married, must be at least 14 years old"):
-        print("ERROR: US10", indi, ": Too young to get married, must be at least 14 years old")
+        print("ERROR: US10", individuals[indi]['NAME'], ": Too young to get married, must be at least 14 years old")
 
 def US01_test_check_current_date(indi, individuals):
     if (US01_check_current_date(indi, individuals) == "Error: date is after the current date"):
-        print("ERROR: US01", indi, ": date is after the current date")
+        print("ERROR: US01", individuals[indi]['NAME'], ": date is after the current date")
 
 def US05_test_married_before_death(indi, individuals):
     if (US05_married_before_death(indi, individuals) == "Error: cannot get married after death"):
-        print("ERROR: US05", indi, ": cannot get married after death")
+        print("ERROR: US05", individuals[indi]['NAME'], ": cannot get married after death")
 
 def US04_test_married_before_div(indi, individuals):
     if (US04_married_before_div(indi, individuals) == "Error: marriage date after divorce date"):
-        print( "ERROR: US04", indi, ": marriage date after divorce date")
+        print( "ERROR: US04", individuals[indi]['NAME'], ": marriage date after divorce date")
 
 #start sprint 2 tests
 def US15_test_child_max(indi, families):
@@ -57,8 +60,8 @@ def US12_test_parents_too_old(indi, individuals, families):
     if US12_parents_too_old(indi, individuals, families):
         print("ERROR US12: ", individuals[indi]['NAME'], " parent(s) too old")
         
-def US16_test_get_last_names(individuals):
-    US16_get_last_names(individuals) 
+def US16_test_get_last_names(indi, individuals):
+    US16_get_last_names(indi, individuals) 
     
 def US30_test_list_living_married(individuals):
     US30_list_living_married(individuals)
@@ -66,11 +69,11 @@ def US30_test_list_living_married(individuals):
 #tests for sprint 3
 def US21_test_gender(indi, families, individuals):
     if (US21_correct_gender(indi, families, individuals) == 'ERROR: US21 wrong gender for role'):
-        print( "ERROR: US21", indi, ": husband or wife is the wrong gender for that role")
+        print( "ERROR: US21 in familiy with ID ", indi, ": husband or wife is the wrong gender for that role")
 
 def US34_test_age_diff(indi, individuals):
     if (US34_age_difference(indi, individuals) == 'ERROR: US34 large age difference in married couple'):
-        print( "ERROR: US34", indi, ": age difference too big, one partner was twice the age of the other when married")
+        print( "ERROR: US34", individuals[indi]['NAME'], ": age difference too big, one partner was twice the age of the other when married")
 
 def US31_test_list_living_single(individuals):
     US31_list_living_single(individuals)
@@ -84,13 +87,13 @@ def US33_test(indi, individuals, families):
 def US22_test(indi, individuals):
     US22_Unique_IDs(indi, individuals)
 
-def US17_test_dont_marry_children(families):
-    if US17_dont_marry_children(families) == "ERROR US17: You cannot marry your children!":
-        print("Error US17: ", marryChild, "You cannot marry your children!")
+def US17_test_dont_marry_children(indi, individuals, families):
+    if US17_dont_marry_children(indi, individuals, families):
+        print("Error US17: ", individuals[indi]['NAME'], " is married to his/her child")
 
-def US18_test_siblings_should_not_marry(individuals,family):
-    if US18_siblings_should_not_marry(individuals,families) == "ERROR US 18: You cannot marry your siblings.":
-        print("ERROR US18: ", dad, "You cannot marry your siblings.")
+def US18_test_siblings_should_not_marry(indi, individuals, families):
+    if US18_siblings_should_not_marry(indi, individuals,families):
+        print("ERROR US18: ", individuals[indi]['NAME'], " is married to his/her sibling")
 
 
 # tests for sprint 4
@@ -102,8 +105,10 @@ def US09_test_born_after_parents_death(indi, individuals, families):
 def US23_test_unique_name_and_birthday(individuals):
     US23_unique_name_and_birthday(individuals)
 
-# call all test functions properly
+#print out individual and family tables
+Parser.print_tables(individuals, families)
 
+# call all test functions properly
 for indi in individuals:
     US01_test_check_current_date(indi, individuals)
     US02_test_born_after_married(indi, individuals)
@@ -117,18 +122,18 @@ for indi in individuals:
     US08_test_born_before_marr(indi, individuals, families)
     US12_test_parents_too_old(indi, individuals, families)
     US09_test_born_after_parents_death(indi, individuals, families)
-    US17_test_dont_marry_children(families)
-    US18_test_siblings_should_not_marry(individuals,family)
+    US17_test_dont_marry_children(indi, individuals, families)
+    US18_test_siblings_should_not_marry(indi, individuals, families)
+    #US16_test_get_last_names(indi, individuals)
+    US33_the_orphans(indi, individuals, families)
 
 for indi in families:
-    US14_test_quin(indi, families)
+    #US14_test_quin(indi, families)
     US15_test_child_max(indi, families)
     US21_test_gender(indi, families, individuals)
 
 US29_the_deceased(individuals)
 US31_test_list_living_single(individuals)
 US32_test_list_multiple_births(individuals, families)
-US33_the_orphans(indi, individuals, families)
 US23_test_unique_name_and_birthday(individuals)
-US16_test_get_last_names(individuals)
 US30_test_list_living_married(individuals)
