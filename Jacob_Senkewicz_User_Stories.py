94ad4e587ee0637284c4397ef256c9ed5dd5a3ef
jakewicz <33581158+jakewicz@users.noreply.github.com>
2020-03-29 11:32:47 -0400

    Update Jacob_Senkewicz_User_Stories.py

--- Jacob_Senkewicz_User_Stories.py
+++ Jacob_Senkewicz_User_Stories.py
@@ -16,13 +16,8 @@ def US16_get_last_names(indi,individuals):
         print(lastName)
     return names
 #____________________________________________US18______________________________________________________________________________
-def US18_siblings_should_not_marry(indi,individuals,families):
+def US30_list_living_married(individuals):
     for indi in individuals:
-        if individuals[indi]['FAMC'] != 'N/A':
-            if individuals[indi]['SPOUSE'] != 'N/A':
-                family_code = individuals[indi]['FAMC']
-                spouse = individuals[indi]['SPOUSE']
-                spouse_family_code = individuals[spouse]['FAMC']
-                if spouse_family_code == family_code:
-                    print("ERROR US 18: You cannot marry your siblings.")
+        if individuals[indi]['FAMS'] != 'N/A' and individuals[indi]['DEAT'] == 'N/A':
+            print(individuals[indi]['NAME'])
 #_____________________________________________________________________________________________________________________________
